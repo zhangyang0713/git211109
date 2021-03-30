aa28a6d9b9a87185d7f28e76f552830bc72cf394
Zhang <zhang@163.com>
2021-03-30 18:49:40 +0800

    hot-fix commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -3,5 +3,7 @@ package com.atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
